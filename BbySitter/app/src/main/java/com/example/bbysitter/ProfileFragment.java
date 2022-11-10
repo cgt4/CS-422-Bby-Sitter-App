c2d02c9856d71fde9da2e4860c81cf4bb0cb03e9
Cristian <cgt4@uic.edu>
2022-11-10 00:09:21 -0600

    Profile functionality is mostly there, it does look ugly though.

--- BbySitter/app/src/main/java/com/example/bbysitter/ProfileFragment.java
+++ BbySitter/app/src/main/java/com/example/bbysitter/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.bbysitter;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,17 +12,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
 
     int arrayIndex;
-    String[] names = new String[]{"Bobby", "Sally", "Sarah", "Madeline", "Heather"};
+    String[] names = new String[]{"Bobby Smith", "Sally Johnson", "Sarah Williams",
+            "Madeline Brown", "Heather Jones"};
     int[] imagePaths = new int[]{R.drawable.babysitterimage0, R.drawable.babysitterimage1,
             R.drawable.babysitterimage2, R.drawable.babysitterimage3, R.drawable.babysitterimage4};
+    float[] ratings = new float[]{4.8f,4.2f,3.9f,3.5f,3.0f};
+    boolean[][] certifications = new boolean[][]{{true, true, true, true},
+        {false, true, true, true},
+        {false, false, true, true},
+        {false, false, false, true},
+        {false, false, false, false}};
     TextView babysitterName;
     ImageView babysitterPicture;
     Button viewProfileButton;
+    RatingBar rating;
+    Button fiveYearsButton;
+    Button cprButton;
+    Button bsButton;
+    Button carButton;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -43,6 +57,23 @@ public class ProfileFragment extends Fragment {
         viewProfileButton.setOnClickListener((View v) ->
                 startActivity(new Intent(this.getContext(), ReviewsDescriptionCredentials.class))
         );
+        rating = view.findViewById(R.id.babysitterRatingBar);
+        rating.setRating(ratings[arrayIndex]);
+        bsButton = view.findViewById(R.id.buttonBS);
+        cprButton = view.findViewById(R.id.buttonCPR);
+        carButton = view.findViewById(R.id.buttonCAR);
+        fiveYearsButton = view.findViewById(R.id.button5Y);
+        for(int j = 0; j < 4; j++) {
+            if (certifications[arrayIndex][0] == false)
+                bsButton.setBackgroundColor(Color.GRAY);
+            if (certifications[arrayIndex][1] == false)
+                cprButton.setBackgroundColor(Color.GRAY);
+            if (certifications[arrayIndex][2] == false)
+                carButton.setBackgroundColor(Color.GRAY);
+            if (certifications[arrayIndex][3] == false)
+                fiveYearsButton.setBackgroundColor(Color.GRAY);
+    }
+
         return view;
     }
 }
