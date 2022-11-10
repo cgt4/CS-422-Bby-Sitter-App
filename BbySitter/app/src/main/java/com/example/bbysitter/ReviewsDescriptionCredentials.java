15268fa538e7d3d12636586a127f4629b770d803
meganmehta <megandeemehta@gmail.com>
2022-11-10 17:24:07 -0600

    added base for extra descriptions on profile

--- BbySitter/app/src/main/java/com/example/bbysitter/ReviewsDescriptionCredentials.java
+++ BbySitter/app/src/main/java/com/example/bbysitter/ReviewsDescriptionCredentials.java
@@ -3,6 +3,7 @@ package com.example.bbysitter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.Button;
 public class ReviewsDescriptionCredentials extends AppCompatActivity {
 
     Button nextPage;
+    Button reviewsDescriptionCredentialsNextPageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,8 @@ public class ReviewsDescriptionCredentials extends AppCompatActivity {
         nextPage.setOnClickListener((View v) ->
                 startActivity(new Intent(this, Reviews.class))
         );
+        setContentView(R.layout.activity_reviews_description_credentials);
+
+
     }
 }
